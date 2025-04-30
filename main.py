b63b5a2f52e0ee33834a856b9f5c4605259d71a7
den1s0v <hello-lumen@ya.ru>
2025-04-30 13:24:27 +0300

    Add async behavior via threading

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import argparse
+import threading
 
 from control import get_shared_folders_managers
 from helpers import duration_report
 
 
-def run(command_name: str):
+def run_command(mgr, command_name):
+    getattr(mgr, command_name).__call__()
+
+def run(command_name: str, use_threads: bool = True):
     mgrs = get_shared_folders_managers()
 
-    with duration_report('all tasks'):
-        for mgr in mgrs:
-            getattr(mgr, command_name).__call__()
+    if use_threads:
+        threads = []
+        with duration_report('all tasks'):
+            for mgr in mgrs:
+                thread = threading.Thread(target=run_command, args=(mgr, command_name))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+    else:
+        with duration_report('all tasks'):
+            for mgr in mgrs:
+                run_command(mgr, command_name)
 
 
 def main():
@@ -25,9 +40,10 @@ def main():
  * stage (from local area to staging area);
  * push (from staging area to remote);
  * dump = stage + push.""")
+    parser.add_argument('--noasync', action='store_true', help='Run commands synchronously')
 
     args = vars(parser.parse_args())
-    run(args['command'])
+    run(args['command'], not args['noasync'])
 
 
 if __name__ == "__main__":
